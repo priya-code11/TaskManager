294082689cc4bb7998d6cf1041aeacf107403bb2
priya-code11 <priya.prajapati5578@gmail.com>
2026-06-27 17:00:41 +0530

    create funcions and endpoints

---
+++ backend/prisma/migrations/20260627094445_create_tasks_table/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "tasks" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT,
+    "status" TEXT NOT NULL DEFAULT 'todo',
+    "priority" TEXT NOT NULL DEFAULT 'medium',
+    "dueDate" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "tasks_pkey" PRIMARY KEY ("id")
+);
