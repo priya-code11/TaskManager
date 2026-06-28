1b5a29f0754bc4f03d7f937d720d62830a255752
Kavy Doshi <kavypdoshi2007@gmail.com>
2026-06-29 00:00:01 +0530

    Add TypeScript definitions for Vite environment variables

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,1 +1,10 @@
+/// <reference types="vite/client" />
 
+interface ImportMetaEnv {
+    readonly VITE_API_BASE_URL: string;
+    // Add any other VITE_ variables you use here
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv;
+}
